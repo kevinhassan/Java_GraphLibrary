c5c88c39916ef14b4b1aee8eef2aeb90f4bddd22
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2016-03-28 15:57:06 +0200

    Add getter/setter Edge

--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -4,4 +4,10 @@ public abstract class Edge()
 {
 	private Vertex v1;
 	private Vertex v2;
+
+	public Vertex getVertexV1();
+	public Vertex getVertexV2();
+	public void setVertexV1(Vertex v1);
+	public void setVertexV2(Vertex v2);
+
 }
