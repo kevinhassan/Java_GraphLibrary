e0f6e4ee15882819d706cb419cec9eba8ff9a7be
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2016-03-28 15:56:47 +0200

    Add getter/setter Graph

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -6,4 +6,9 @@ public interface Graph
 {
 	ArrayList<Vertext> vertexs = new ArrayList();
 	ArrayList<Edge> edges = new ArrayList();
+
+	public void setVertex(Vertex v);
+	public void setEdge(Edge e);
+	public Vertex getVertexs();
+	public Edge getEdges();
 }
