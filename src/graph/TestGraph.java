7b2d7721af521834316f96a6527753943f2335ec
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2016-03-28 14:55:15 +0200

    Implement main TestGraph

--- src/graph/TestGraph.java
+++ src/graph/TestGraph.java
@@ -2,5 +2,11 @@ package graph;
 
 public class TestGraph()
 {
-
+	public static void main(String[] args)
+	{
+		Vertex v1 = new Vertex("sommet 1");
+		Vertex v2 = new Vertex("sommet 2");
+		Edge e1 = new Edge(v1,v2);
+		Graph g;
+	}
 }
