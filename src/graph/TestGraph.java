919e109268ebb27a9dbc7fe14686765a9ea2f884
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2016-04-02 15:59:54 +0200

    Add & Implementation of new Exceptions

--- src/graph/TestGraph.java
+++ src/graph/TestGraph.java
@@ -2,15 +2,13 @@ package graph;
 
 public class TestGraph implements Graph
 {
-	Vertex[] vertexs;
-	Edge[] edges;
-	public void setVertex(Vertex v)
+	private Vertex[] vertexs;
+	private Edge[] edges;
+	public void setVertex(Vertex v) throws MaximumSizeReachedOnVertices
 	{
-
 	}
-	public void setEdge(Edge e)
-	{
-
+	public void setEdge(Edge e) throws NotUndirectedEdge, MaximumSizeReachedOnEdges
+	{ 
 	}
 	public Vertex[] getVertexs()
 	{
@@ -19,6 +17,43 @@ public class TestGraph implements Graph
 	public Edge[] getEdges()
 	{
 		return this.edges;
+	}
+
+	public class MaximumSizeReachedOnEdges extends Exception 
+	{
+    	public String toString() {
+        	return "Vous avez atteint le nombre maximum d'arêtes sur ce graphe.";
+    	}
+    }
+    public class MaximumSizeReachedOnVertices extends Exception 
+    {
+    	public String toString() {
+        	return "Vous avez atteint le nombre maximum de sommets sur ce graphe.";
+    	}
+	}
+	public class MinimumSizeOfGraphNotRespected extends Exception 
+	{
+
+	    private int maxEdges;
+	    private int maxVertices;
+
+	    public MinimumSizeOfGraphNotRespected(int maxEdges, int maxVertices) 
+	    {
+	        this.maxEdges = maxEdges;
+	        this.maxVertices = maxVertices;
+	    }
+
+	    public String toString() 
+	    {
+	        return "Il est necessaire d'avoir au moins 1 arête et 2 sommets, alors que vous n'avez autorisez que "
+	                + this.maxEdges + " arêtes et " + this.maxVertices + " sommets.";
+	    }
+	}
+	public class NotUndirectedEdge extends Exception 
+	{
+	    public String toString() {
+	        return "Vous essayez d'ajouter une arête qui est dirigée à un graphe qui ne l'est pas.";
+	    }
 	}
 
 	public static void main(String[] args)
