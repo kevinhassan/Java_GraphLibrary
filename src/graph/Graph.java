21a9d3ebe26befc179b1af6e2a69be67b75edab9
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2016-04-02 17:50:16 +0200

    Fix Interface Throw

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -2,6 +2,7 @@ package graph;
 
 import graph.Vertex;
 import graph.Edge;
+import MyExceptions.*;
 /**
 *	The Graph interface gather all others class in array Structure
 */
@@ -11,12 +12,12 @@ public interface Graph
 	* Add the Vertex to the Vertex arrayList
 	* @param Vertex 
 	*/
-	public void setVertex(Vertex v) throws MaximumSizeReachedOnVertices;
+	public void setVertex(Vertex v);
 	/**
 	* Add the Edge to the Edge array
 	* @param Edge 
 	*/
-	public void setEdge(Edge e) throws NotUndirectedEdge, MaximumSizeReachedOnEdges;
+	public void setEdge(Edge e);
 	/**
 	* Returns the array of Vertex 
 	* @return Vertex 
