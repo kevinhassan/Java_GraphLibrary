49d6827ce5ee4371f5ee68e08ed37fbfb73741db
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2016-04-06 00:45:18 +0200

    Add Manifest.mf & Add  TestImpl.java

---
+++ Implementation/src/TestImpl.java
@@ -0,0 +1,22 @@
+package graph;
+
+public class TestImpl 
+{
+	public void main(String[] args)
+	{
+		ImplementGraph g = new ImplementGraph();
+		Vertex v1 = new Vertex();
+		Vertex v2 = new Vertex();
+		Vertex v3 = new Vertex();
+		Vertex v4 = new Vertex();
+		g.addVertex(v1);
+		g.addVertex(v2);
+		g.addVertex(v3);
+		g.addVertex(v4);
+		UndirectedEdge dirE = new UndirectedEdge(v1,v2);
+		DirectedEdge undirE = new DirectedEdge(v3,v4);
+		g.addDirectedEdge(dirE);
+		g.addUndirectedEdge(undirE);
+		Vertex[] v = getNeighbours(v1); 
+	}
+}
