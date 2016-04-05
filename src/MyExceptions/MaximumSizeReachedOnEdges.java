2d36c1a6064942e551b8cccdf5348e0dc2bcb3d1
Kevin HASSAN <kevin.hassan@igtp4-mac13.local>
2016-04-05 16:59:10 +0200

    Move Folders & Files

--- src/MyExceptions/MaximumSizeReachedOnEdges.java
+++
@@ -1,8 +0,0 @@
-	package MyExceptions;
-	
-	public class MaximumSizeReachedOnEdges extends Exception 
-	{
-		public String toString() {
-	    	return "Vous avez atteint le nombre maximum d'arêtes sur ce graphe.";
-		}
-	}
--- src/MyExceptions/MaximumSizeReachedOnVertices.java
+++
@@ -1,8 +0,0 @@
-package MyExceptions;
-
-public class MaximumSizeReachedOnVertices extends Exception 
-{
-	public String toString() {
-    	return "Vous avez atteint le nombre maximum de sommets sur ce graphe.";
-	}
-}
